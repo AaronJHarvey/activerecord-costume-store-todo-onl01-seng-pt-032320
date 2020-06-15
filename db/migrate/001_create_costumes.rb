29d31e680825c15b05826a0b019ddf73db193e64
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-06-15 01:20:48 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,12 +1,12 @@
-class CreateCostumes < ActiveRecord::Migration[5.2]
+class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
-  create_table :costumes do |c|
-    c.string :name 
-    c.float :price 
-    c.string :image_url
-    c.string :size 
-    
-     t.timestamps null: false
+    create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+
+      t.timestamps null: false
     end
-  end 
-end 
+  end
+end
